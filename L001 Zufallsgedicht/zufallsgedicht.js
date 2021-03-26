1d2c11bb127e569378413cd5333e263fc6f5a449
JuliaKaeppeler <julia.helena.kaeppeler@hs-furtwangen.de>
2021-03-26 11:07:13 +0100

    finish

--- L001 Zufallsgedicht/zufallsgedicht.js
+++ L001 Zufallsgedicht/zufallsgedicht.js
@@ -1,21 +1,21 @@
 var Zufallsgedicht;
 (function (Zufallsgedicht) {
     var subject = ["Snape", "Hermine", "Harry", "Ron", "Hagrid", "Dumbledore"];
-    var praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstoert"];
+    var predicate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstoert"];
     var object = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     // console.log(subject, praedikat, object);
     for (var i = 6; i > 1; i--) {
         // console.log(i);
-        getVerse(subject, praedikat, object);
+        getVerse(subject, predicate, object);
     }
-    function getVerse(_subject, _praedikat, _object) {
+    function getVerse(_subject, _predicate, _object) {
         // console.log("Alohomora");
         var verse = "";
-        var zufallSubject = Math.floor(_subject.length * Math.random());
-        console.log(zufallSubject);
-        var zufallPraedikat = Math.floor(_praedikat.length * Math.random());
-        var zufallObject = Math.floor(_object.length * Math.random());
-        verse = _subject.splice(zufallSubject, 1)[0] + " " + _praedikat.splice(zufallPraedikat, 1)[0] + " " + _object.splice(zufallObject, 1)[0];
+        var randomSubject = Math.floor(_subject.length * Math.random());
+        console.log(randomSubject);
+        var randomPredicate = Math.floor(_predicate.length * Math.random());
+        var randomObject = Math.floor(_object.length * Math.random());
+        verse = _subject.splice(randomSubject, 1)[0] + " " + _predicate.splice(randomPredicate, 1)[0] + " " + _object.splice(randomObject, 1)[0];
         console.log(verse);
     }
 })(Zufallsgedicht || (Zufallsgedicht = {}));
--- L001 Zufallsgedicht/zufallsgedicht.ts
+++ L001 Zufallsgedicht/zufallsgedicht.ts
@@ -1,26 +1,26 @@
 namespace Zufallsgedicht {
     let subject: string[] = ["Snape", "Hermine", "Harry", "Ron", "Hagrid", "Dumbledore"];
-    let praedikat: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstoert"];
+    let predicate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstoert"];
     let object: string[] = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
     // console.log(subject, praedikat, object);
 
     for (let i: number = 6; i > 1; i--) {
        // console.log(i);
-        getVerse(subject, praedikat, object);
+        getVerse(subject, predicate, object);
     }
 
-    function getVerse(_subject: string[], _praedikat: string[], _object: string[]): void {
+    function getVerse(_subject: string[], _predicate: string[], _object: string[]): void {
         // console.log("Alohomora");
         let verse: string = "";
         
-        let zufallSubject: number = Math.floor(_subject.length * Math.random());
-        console.log(zufallSubject);
+        let randomSubject: number = Math.floor(_subject.length * Math.random());
+        console.log(randomSubject);
         
-        let zufallPraedikat: number = Math.floor(_praedikat.length * Math.random());
-        let zufallObject: number = Math.floor(_object.length * Math.random());
+        let randomPredicate: number = Math.floor(_predicate.length * Math.random());
+        let randomObject: number = Math.floor(_object.length * Math.random());
        
-        verse = _subject.splice(zufallSubject, 1)[0] + " " + _praedikat.splice(zufallPraedikat, 1)[0] + " " + _object.splice(zufallObject, 1)[0];
+        verse = _subject.splice(randomSubject, 1)[0] + " " + _predicate.splice(randomPredicate, 1)[0] + " " + _object.splice(randomObject, 1)[0];
         console.log(verse);
     }
 }
